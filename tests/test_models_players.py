ef345dff4fb523bc450ec4e4de77fe02a559d00f
ple <12067707+poulpe@users.noreply.github.com>
2025-01-31 21:28:53 +0100

    updated tests

--- tests/test_models_players.py
+++ tests/test_models_players.py
@@ -1,18 +1,12 @@
-from time import sleep
-
-# from uuid import uuid4
-#
-# import pytest
-
 from padel_tracker.models.players import Player
 
 
 def test_player_creation():
     yes = Player(name="Coucou")
-    print(yes)
-    sleep(0.5)
+    assert yes.name == "Coucou"
+    assert yes.id is not None
+    assert yes.nb_matches == 0
     yes.elo_rating = 1200
-    print(yes.id)
 
 
 def test_team_creation():
