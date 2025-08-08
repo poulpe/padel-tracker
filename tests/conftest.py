65ab8e9f8b7da744c56bb8df1f253251f520e365
poulpe <12067707+poulpe@users.noreply.github.com>
2025-08-08 19:28:18 +0200

    fix: force "test" mode to be settled during tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,16 @@
 import pytest
 
-# from padel_tracker.utils.conf import DBMode, RunMode
+from padel_tracker.utils.conf import DBMode, RunMode
 from padel_tracker.utils.paths import APP_PATH as APP_PATH
-from padel_tracker.database.db import DB, init_db_and_tables
+from padel_tracker.database.db import Database, init_db_and_tables
 
 # Database
-
+DB_TEST = Database(db_mode=DBMode.LOCAL, run_mode=RunMode.TEST)
 
 @pytest.fixture
 def db_session():
-    init_db_and_tables(DB)
-    session = DB.get_session()
+    init_db_and_tables(DB_TEST)
+    session = DB_TEST.get_session()
     yield session
     session.close()
 
