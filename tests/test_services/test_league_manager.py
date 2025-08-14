019c78bc8780fe23c764a25d11820f7ddae4ddbb
poulpe <12067707+poulpe@users.noreply.github.com>
2025-08-14 23:21:59 +0200

    Added tests on services + fixtures

--- tests/test_services/test_league_manager.py
+++ tests/test_services/test_league_manager.py
@@ -1,21 +1,72 @@
-from padel_tracker.services.league_manager import (
-    create_league,
-    get_league_from_name,
-    delete_league,
-)
+import pytest
+
+from padel_tracker.utils.errors import LeagueNotFoundError
+from padel_tracker.services import league_manager
 
 
 def test_create_get_delete_league(db_session):
     league_name = "CrashTest League"
     # Create
-    league = create_league(db_session, name=league_name, is_private=True)
+    league = league_manager.create_league(db_session, name=league_name, is_private=True)
     assert league.name == league_name
     assert league.is_private is True
     assert league.nb_matches == 0
     assert league.nb_players == 0
     # Get
-    found_league = get_league_from_name(db_session, league_name)
+    found_league = league_manager.get_league_from_name(db_session, league_name)
     assert found_league is not None
     assert found_league.name == league_name
     # Delete
-    delete_league(db_session, league_name)
+    league_manager.delete_league(db_session, league_name)
+
+
+def test_delete_unexisting_league(db_session):
+    with pytest.raises(LeagueNotFoundError):
+        league_manager.delete_league(db_session, "Nope this league is not real")
+
+
+def test_add_player_to_league(
+    db_session, make_dummy_user, make_dummy_player, make_dummy_league
+):
+    # Create admin user/player of league #1
+    admin1_name = "Le chef"
+    make_dummy_user(name=admin1_name, is_create_player=True)
+    # Create league #1
+    league1_name = "Super cool league"
+    league1 = make_dummy_league(
+        name=league1_name, is_private=True, admin_name=admin1_name
+    )
+
+    # Create admin player of league #2
+    admin2_name = "Bof le chef"
+    make_dummy_user(name=admin2_name, is_create_player=True)
+    # Create league #2
+    league2_name = "Less cool league"
+    league2 = make_dummy_league(
+        name=league2_name, is_private=True, admin_name=admin2_name
+    )
+
+    # Create players in league #1 and league #2
+    player2_name = "Follower Dos"
+    player3_name = "Follower Tres"
+    player4_name = "Follower Cuatro"
+    for name in [player2_name, player3_name, player4_name]:
+        make_dummy_player(name=name, league=[league1, league2])
+
+    # Check players in league #1
+    list_links_league1 = league_manager.get_linkplayerleague_from_league(
+        db_session, league1_name
+    )
+    list_player_names_league1 = [link.player_name for link in list_links_league1]
+    for player_name in [admin1_name, player2_name, player3_name, player4_name]:
+        assert player_name in list_player_names_league1
+
+    # Check players in league #2
+    list_links_league2 = league_manager.get_linkplayerleague_from_league(
+        db_session, league2_name
+    )
+    list_player_names_league2 = [link.player_name for link in list_links_league2]
+    for player_name in [admin2_name, player2_name, player3_name, player4_name]:
+        assert player_name in list_player_names_league2
+
+    # No needs to delete and clean, already managed by fixtures
--- tests/test_services/test_user_manager.py
+++ tests/test_services/test_user_manager.py
@@ -1,21 +1,107 @@
-from tests.conftest import TEST_P1_NAME, TEST_LEAGUE_NAME
-from padel_tracker.utils.errors import UserExistsError
-from padel_tracker.services import user_manager
+import pytest
+
+from padel_tracker.utils.errors import (
+    UserExistsError,
+    UserNotFoundError,
+    PlayerExistsError,
+    LeagueNotFoundError,
+    PlayerNotFoundError,
+)
+from padel_tracker.database.db import commit_to_db
+from padel_tracker.models.users import UserRole
+from padel_tracker.services import user_manager, player_manager, league_manager
+from tests.conftest import TEST_LEAGUE_NAME
 
 
-def test_create_get_user(db_session):
+def test_create_get_delete_user(db_session):
+    # Prepare test
     auth_user_id = "auth123"
-    user_name = TEST_P1_NAME
+    user_name = "NewUser"
     dict_auth_user = {"sub": auth_user_id, "name": user_name}
+    # Check it doesn't exist already (and if so, delete it)
     try:
-        user = user_manager.create_user_from_auth_user(
-            db_session,
-            dict_auth_user=dict_auth_user,
-            is_create_player=False,
-            default_league_name=TEST_LEAGUE_NAME,
-        )
-    except UserExistsError:
         user = user_manager.get_user_from_auth_user_id(db_session, auth_user_id)
+        user_manager.delete_user(db_session, name=user.name)
+    except UserNotFoundError:
+        pass
+
+    # Create user
+    user = user_manager.create_user_from_auth_user(
+        db_session, dict_auth_user=dict_auth_user, is_create_player=False
+    )
     assert user.auth_user_id == auth_user_id
     assert user.name == user_name
+    ## Check error while recreating it
+    with pytest.raises(UserExistsError):
+        user_manager.create_user_from_auth_user(
+            db_session, dict_auth_user=dict_auth_user, is_create_player=False
+        )
+
+    # Get it and update
+    with db_session as session:
+        user = user_manager.get_user_from_auth_user_id(session, auth_user_id)
+        ## Assert basic features with player
+        try:
+            new_player = player_manager.create_player(session=session, name=user_name)
+        except PlayerExistsError:
+            new_player = player_manager.get_player_from_name(session, user_name)
+        user_manager.assign_player_to_user(session, user, new_player)
+        assert user.player.name == user_name
+        assert user.player.nb_matches == 0
+        ## Update and check well updated
+        user.nb_visits += 1
+        user.role = UserRole.TRUSTEDPLAYER
+        commit_to_db(user, session=session)
+    with db_session as session:
+        updated_user = user_manager.get_user_from_auth_user_id(session, auth_user_id)
+        assert updated_user in user_manager.get_all_users(session=session)
+        assert updated_user.nb_visits == 2
+        assert updated_user.role == UserRole.TRUSTEDPLAYER
+
+    # Delete
+    user_manager.delete_user(session=db_session, name=user_name)
+    with pytest.raises(UserNotFoundError):
+        user_manager.delete_user(session=db_session, name=user_name)
+    player_manager.delete_player(session=db_session, name=user_name)
+
+
+def test_create_get_delete_user_with_player(db_session):
+    # Prepare test
+    auth_user_id = "auth666"
+    user_name = "NewUser NewPlayer"
+    dict_auth_user = {"sub": auth_user_id, "name": user_name}
+    ## Check user doesn't exist already (and if so, delete it)
+    try:
+        user = user_manager.get_user_from_auth_user_id(db_session, auth_user_id)
+        user_manager.delete_user(db_session, name=user.name)
+    except UserNotFoundError:
+        pass
+    ## Check player doesn't exist already (and if so, delete it)
+    try:
+        player_manager.get_player_from_name(db_session, user_name)
+        player_manager.delete_player(db_session, name=user_name)
+    except PlayerNotFoundError:
+        pass
+
+    # Create user
+    ## Ensure the league exists
+    try:
+        league_manager.get_league_from_name(db_session, TEST_LEAGUE_NAME)
+    except LeagueNotFoundError:
+        league_manager.create_league(db_session, name=TEST_LEAGUE_NAME)
+    ## Go
+    user = user_manager.create_user_from_auth_user(
+        db_session,
+        dict_auth_user=dict_auth_user,
+        is_create_player=True,
+        default_league_name=TEST_LEAGUE_NAME,
+    )
+    assert user.player.name == user_name
+    assert user.player.nb_matches == 0
+    assert user.player_id == user.player.id
     assert user.default_league_name == TEST_LEAGUE_NAME
+    assert user.nb_visits == 1
+
+    # Delete
+    user_manager.delete_user(session=db_session, name=user_name)
+    player_manager.delete_player(session=db_session, name=user_name)
